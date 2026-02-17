b2ab33429dc94810db00cb085278c962b55d3062
ivanfiddy <ivanfidelskiy@gmail.com>
2026-02-17 15:21:39 +0400

    Update ORFs.py
    Added a bandgraph with matplot lib

--- srs/ORFs.py
+++ srs/ORFs.py
@@ -50,6 +50,8 @@ class AnalyzeORFs:
             self._aminos5bit[i::5].__iter__() for i in range(5)
         )
 
+        overlap_indexes = []
+
         orfs_indexes = []
 
         running_orf = False
@@ -66,6 +68,9 @@ class AnalyzeORFs:
                 if i == start_codon:
                     index_overlap += 1
                     orfs_indexes.append([running_pos, None])
+                    overlap_indexes.append(
+                        len(orfs_indexes) - 1
+                    )
 
                 elif i == stop_codon:
 
@@ -91,15 +96,66 @@ class AnalyzeORFs:
 
         self.orfs_indexes = pnd.DataFrame(tuple(orfs_indexes), columns=['start', 'stop'])
         self.orfs_indexes['length'] = self.orfs_indexes['stop'] - self.orfs_indexes['start']
+        self.orfs_indexes['overlap'] = False
+        self.orfs_indexes.loc[overlap_indexes, 'overlap'] = True
+
 
     ### Band graph
 
     @_checkifbuilt
-    def bandgraph(self):
+    def bandsgraph(self, start=0, stop=None, chart_height=10, chart_width=10, overlap=False):
 
-        
+        indexes = self.orfs_indexes[
+            self.orfs_indexes['overlap'] == False
+        ]
 
-        pass
+        per_row = round(indexes.__len__() / chart_height)
+
+        slices = (slice(i*per_row, ((i+1)*per_row)+1)
+                  for i in range(chart_height))
+
+
+        fig, ax = plt.subplots()
+
+
+        y_pos_start = -0.2
+        some_argument_idk = 0.8
+        prev = 0
+
+        for y_pos in range(chart_height):
+
+            line_bars = indexes.iloc[next(slices)]
+
+            bar_data = line_bars.loc[:, ['start', 'length']]
+
+            last_data = bar_data.iloc[-1]
+
+            row_data_length = last_data['start'] + last_data['length']
+
+            # I feel like this is not written optimally or most concisley
+            bar_data['start'] = bar_data['start'].map(lambda x: float((x - prev) / (row_data_length - prev) * chart_width))
+            bar_data['length'] = bar_data['length'].map(lambda x: float(x / (row_data_length - prev) * chart_width))
+
+            bar_plot = bar_data.itertuples(index=False, name=None)
+
+            ax.broken_barh(tuple([*bar_plot]), (y_pos_start, some_argument_idk), picker=True).set_gid(line_bars.to_dict(orient='records'))
+
+            y_pos_start += 1
+            prev = row_data_length
+
+        ax.set_yticks(range(chart_height))
+        ax.invert_yaxis()
+        ax.set_xlim(0, chart_width)
+
+        plt.show()
+
+
+
+
+
+
+
+
 
 
     ### Private methods
@@ -134,15 +190,3 @@ class AnalyzeORFs:
             return tuple(orfs)
 
         pass
-
-
-
-
-
-
-
-
-
-
-
-
